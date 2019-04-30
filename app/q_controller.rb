f96208abdf5e8ac097cb6415699117db3d95c107
ahenning <ahenning@leapfrogonline.com>
2019-04-30 15:54:04 -0500

    Added views

--- app/q_controller.rb
+++ app/q_controller.rb
@@ -2,4 +2,14 @@ class QController < R00lz::Controller
   def a_quote
     "The night is dark and full of terrors"
   end
+
+  def shakes
+    @noun = :winking
+    render(:shakes)
+  end
+
+  def card_trick
+    n = params["card"] || "Queen"
+    "Your card: the #{n} of spades!"
+  end
 end
